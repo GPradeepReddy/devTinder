013520b2ed7b527b470fa8cf248d3048731cb4f3
pgangammagari <pradeepg4393@gmail.com>
2025-04-26 23:42:51 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,13 +2,32 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Pradeep", lastName: "Reddy" });
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("user first response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("user second response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("user third response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("user fourth response");
+    next();
+  },
+  (req, res) => {
+    console.log("user fifth response");
+    res.send({ firstName: "Pradeep", lastName: "Reddy" });
+  }
+);
 
 app.post("/user", async (req, res) => {
   console.log(req.body);
-
   res.send("User data saved successfully");
 });
 
@@ -20,16 +39,16 @@ app.use("/test", (req, res) => {
   res.send("Response form the server!");
 });
 
-// Query params 
+// Query params
 app.get("/search", (req, res) => {
   const { name, age } = req.query;
   res.send(`Searching for ${name} and ${age}`);
 });
 
 // Dynamic routes
-app.get("/users/:userId", (req, res) => {
-  const { userId } = req.params;
-  res.send(`User ID ${userId}`);
+app.get("/users/:userId/:name/:password", (req, res) => {
+  const { userId, name, password } = req.params;
+  res.send(`User ID ${userId} ${name} ${password}`);
 });
 
 app.listen(7777, () => {
