774c704ac1949cd19a3f632b8f22a575af961254
pgangammagari <pradeepg4393@gmail.com>
2025-04-28 15:37:31 +0530

    get, update and delete API's

--- src/app.js
+++ src/app.js
@@ -5,34 +5,76 @@ const connectDB = require("./config/database.js");
 const app = express();
 const User = require("./models/user.js");
 
+app.use(express.json()); // To parse JSON body
+
 app.post("/signup", async (req, res) => {
-
-  const user = new User({
-    firstName: "Pradeep",
-    lastName: "Reddy",
-    email: "pradeep@gmail.com",
-    password: "123",
-  });
-
   try {
+    const { firstName, lastName, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).send("User already existing");
+    const user = new User({ firstName, lastName, email, password });
     await user.save();
-    res.send("user added successfully");
+    res.send("User added successfully");
   } catch (err) {
     res.status(400).send("error on saving the user " + err.message);
   }
 });
 
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Pradeep", lastName: "Reddy" });
+app.get("/user", async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).send("User not found");
+    res.send(user);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
-app.post("/user", userAuth, async (req, res) => {
-  console.log(req.body);
-  res.send("User data saved successfully");
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    if (!users) return res.status(404).send("Users not found");
+    res.send(users);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
-app.delete("/user", (req, res) => {
-  res.send("User deleted successfully");
+app.patch("/user/:id", userAuth, async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!user) return res.status(404).send("User not found");
+    res.send(user);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.patch("/user/email/:email", async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate(
+      { email: req.params.email },
+      req.body,
+      { new: true }
+    );
+    if (!user) return res.status(404).send("user not found");
+    res.send(user);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.delete("/user/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(404).send("user not found");
+    res.send("User deleted successfully");
+  } catch {
+    res.status(500).send(err.message);
+  }
 });
 
 // Query params
@@ -42,9 +84,14 @@ app.get("/search", (req, res) => {
 });
 
 // Dynamic routes
-app.get("/users/:userId/:name/:password", (req, res) => {
-  const { userId, name, password } = req.params;
-  res.send(`User ID ${userId} ${name} ${password}`);
+app.get("/user/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).send("user not found");
+    res.send(user);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
 //Global erorr handling
--- src/models/user.js
+++ src/models/user.js
@@ -12,10 +12,11 @@ const userSchema = new Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
-    required: true,
+    required: true, 
   },
   age: {
     type: Number,
