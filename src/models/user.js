17dee3bf54b1659fbfda18f173410200ae87385b
pgangammagari <pradeepg4393@gmail.com>
2025-04-30 21:21:16 +0530

    exploring validator library

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,11 @@
 const { Schema, model } = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new Schema(
   {
     firstName: {
       type: String,
-      required: [true, 'Name must be required'],
+      required: [true, "Name must be required"],
       minLength: 4,
       maxLength: 24,
     },
@@ -17,11 +18,21 @@ const userSchema = new Schema(
       unique: true,
       trim: true,
       lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is not valid " + value);
+        }
+      },
     },
     password: {
       type: String,
       required: true,
-      minLength: [6, 'Password minimum 6 charecters']
+      minLength: [6, "Password minimum 6 charecters"],
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Week password " + value);
+        }
+      },
     },
     age: {
       type: Number,
@@ -38,6 +49,11 @@ const userSchema = new Schema(
     photoUrl: {
       type: String,
       default: "https://geographyandyou.com/images/user-profile.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid URL " + value);
+        }
+      },
     },
     about: {
       type: String,
