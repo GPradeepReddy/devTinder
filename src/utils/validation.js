d68522fc3474fb8ca6248502fc2b61f3d834343e
pgangammagari <pradeepg4393@gmail.com>
2025-05-02 21:31:20 +0530

    logout + edit profile API

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -7,14 +7,36 @@ const validateSignupData = (req) => {
   if (!firstName || !lastName) {
     throw new Error("Name is not valid");
   }
+
   if (!validator.isEmail(email)) {
     throw new Error("Email is not valid");
   }
+
   if (!validator.isStrongPassword(password)) {
     throw new Error("Please Enter strong password");
   }
 };
 
+const validateEditProfileData = (req) => {
+  const allowEditFields = [
+    "firstName",
+    "lastName",
+    "email",
+    "about",
+    "age",
+    "skills",
+    "gender",
+    "photoUrl",
+  ];
+
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowEditFields.includes(field)
+  );
+
+  return isEditAllowed;
+};
+
 module.exports = {
   validateSignupData,
+  validateEditProfileData
 };
